91f07ad3113e1a660bccb90984d25fc20b5563f9
Samantha Callie <Hoomee90@gmail.com>
2024-09-04 14:49:46 -0700

    add message validation

--- MessageClient/Controllers/MessagesController.cs
+++ MessageClient/Controllers/MessagesController.cs
@@ -5,52 +5,66 @@ namespace MessageClient.Controllers;
 
 public class MessagesController : Controller
 {
-  public IActionResult Index()
-  {
-    List<Message> messages = Message.GetMessages();
-    return View(messages);
-  }
+	public IActionResult Index()
+	{
+		List<Message> messages = Message.GetMessages();
+		return View(messages);
+	}
 
-  public IActionResult Details(int id)
-  {
-    Message message = Message.GetDetails(id);
-    return View(message);
-  }
+	public IActionResult Details(int id)
+	{
+		Message message = Message.GetDetails(id);
+		return View(message);
+	}
 
-  public ActionResult Create()
-  {
-    return View();
-  }
+	public ActionResult Create()
+	{
+		return View();
+	}
 
-  [HttpPost]
-  public ActionResult Create(Message message)
-  {
-    Message.Post(message);
-    return RedirectToAction("Index");
-  }
+	[HttpPost]
+	public ActionResult Create(Message message)
+	{
+		if (!ModelState.IsValid)
+		{
+			return View(message);
+		}
+		else
+		{
+			Message.Post(message);
+			return RedirectToAction("Index");
+		}
+	}
 
-  public ActionResult Edit(int id)
-  {
-    Message message = Message.GetDetails(id);
-    return View(message);
-  }
+	public ActionResult Edit(int id)
+	{
+		Message message = Message.GetDetails(id);
+		return View(message);
+	}
 
-  [HttpPost]
-  public ActionResult Edit(Message message)
-  {
-    Message.Put(message);
-    return RedirectToAction("Details", new { id = message.MessageId });
-  }
-  public ActionResult Delete(int id)
-  {
-    Message message = Message.GetDetails(id);
-    return View(message);
-  }
+	[HttpPost]
+	public ActionResult Edit(Message message)
+	{
+		if (!ModelState.IsValid)
+		{
+			return View(message);
+		}
+		else
+		{
+			Message.Put(message);
+			return RedirectToAction("Details", new { id = message.MessageId });
+		}
+	}
+	public ActionResult Delete(int id)
+	{
+		Message message = Message.GetDetails(id);
+		return View(message);
+	}
 
-  [HttpPost, ActionName("Delete")]
-  public ActionResult DeleteConfirmed(int id)
-  {
-    Message.Delete(id);
-    return RedirectToAction("Index");
-  }
+	[HttpPost, ActionName("Delete")]
+	public ActionResult DeleteConfirmed(int id)
+	{
+		Message.Delete(id);
+		return RedirectToAction("Index");
+	}
 }
--- MessageClient/Models/Message.cs
+++ MessageClient/Models/Message.cs
@@ -1,51 +1,56 @@
+using System.ComponentModel.DataAnnotations;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 
 namespace MessageClient.Models;
 public class Message
 {
-  public int MessageId { get; set; }
-  public string Username { get; set; }
-  public string Content { get; set; }
-  public DateTime PostedOn { get; set; }
-  public int BoardId { get; set; }
+	public int MessageId { get; set; }
+	[Required]
+	public string Username { get; set; }
+	[Required]
+	public string Content { get; set; }
+	[Required]
+	public DateTime PostedOn { get; set; }
+	[Required]
+	public int BoardId { get; set; }
 
-  public static List<Message> GetMessages()
-  {
-    var apiCallTask = ApiHelper.GetAll();
-    var result = apiCallTask.Result;
+	public static List<Message> GetMessages()
+	{
+		var apiCallTask = ApiHelper.GetAll();
+		var result = apiCallTask.Result;
 
-    JArray jsonResponse = JsonConvert.DeserializeObject<JArray>(result);
-    List<Message> messageList = JsonConvert.DeserializeObject<List<Message>>(jsonResponse.ToString());
+		JArray jsonResponse = JsonConvert.DeserializeObject<JArray>(result);
+		List<Message> messageList = JsonConvert.DeserializeObject<List<Message>>(jsonResponse.ToString());
 
-    return messageList;
-  }
+		return messageList;
+	}
 
-  public static Message GetDetails(int id)
-  {
-    var apiCallTask = ApiHelper.Get(id);
-    var result = apiCallTask.Result;
+	public static Message GetDetails(int id)
+	{
+		var apiCallTask = ApiHelper.Get(id);
+		var result = apiCallTask.Result;
 
-    JObject jsonResponse = JsonConvert.DeserializeObject<JObject>(result);
-    Message message = JsonConvert.DeserializeObject<Message>(jsonResponse.ToString());
+		JObject jsonResponse = JsonConvert.DeserializeObject<JObject>(result);
+		Message message = JsonConvert.DeserializeObject<Message>(jsonResponse.ToString());
 
-    return message;
-  }
+		return message;
+	}
 
-  public static void Post(Message message)
-  {
-    string jsonMessage = JsonConvert.SerializeObject(message);
-    ApiHelper.Post(jsonMessage);
-  }
+	public static void Post(Message message)
+	{
+		string jsonMessage = JsonConvert.SerializeObject(message);
+		ApiHelper.Post(jsonMessage);
+	}
 
-  public static void Put(Message message)
-  {
-    string jsonMessage = JsonConvert.SerializeObject(message);
-    ApiHelper.Put(message.MessageId, jsonMessage);
-  }
+	public static void Put(Message message)
+	{
+		string jsonMessage = JsonConvert.SerializeObject(message);
+		ApiHelper.Put(message.MessageId, jsonMessage);
+	}
 
-  public static void Delete(int id)
-  {
-    ApiHelper.Delete(id);
-  }
+	public static void Delete(int id)
+	{
+		ApiHelper.Delete(id);
+	}
 }
